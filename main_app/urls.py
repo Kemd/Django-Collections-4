566c12513c2fc145a29b309b929c8f1403f08e19
kemd <kemd91@gmail.com>
2022-07-09 11:56:55 -0400

    adds content to main and about pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     # main path/url 
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    # route for games 
+    path('games/', views.games_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Hello')
+
+def about(request):
+    return render(request, 'about.html')
+
+# test data for games 
+class Game:
+    def __init__(self, name, year, desc, genre, platform):
+        self.name = name
+        self.year = year
+        self.desc = desc
+        self.genre = genre
+        self.platform = platform
+
+games = [
+    Game('Elden Ring', 2022, 'Foul tarnished, put these foolish ambitions to rest', 'Action, Adventure, RPG', 'PC, PS4/PS5, Xbox'),
+    Game('Warframe', 2013, 'Space Ninja', 'Action, Adventure, Multiplayer RPG', 'PC'),
+    Game('Rocket League', 2015, 'Soccer, but with cars', 'Sport', 'PS4')
+]
+
+# game index page 
+def games_index(request):
+    return render(request, 'games/index.html', {'games': games})
